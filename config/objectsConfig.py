9033e66fe0d0c223a4a79809be46834b9f322ded
codex-yv <yourajverma960@gmail.com>
2025-11-13 03:23:14 +0530

    switched object to deployment

--- config/objectsConfig.py
+++ config/objectsConfig.py
@@ -1,7 +1,7 @@
-# class FixedObjectID(): # for deployment
-#     redirect = "691133cb2b3d2191a187a61a"
-#     ticVer = "691134122b3d2191a187a61d"
+class FixedObjectID(): # for deployment
+    redirect = "691133cb2b3d2191a187a61a"
+    ticVer = "691134122b3d2191a187a61d"
 
-class FixedObjectID(): # for development
-    redirect = "690f8c2a5d7cf53e815adf0d"
-    ticVer = "6910e37a2b3d2191a187a5d6"
+# class FixedObjectID(): # for development
+#     redirect = "690f8c2a5d7cf53e815adf0d"
+#     ticVer = "6910e37a2b3d2191a187a5d6"
